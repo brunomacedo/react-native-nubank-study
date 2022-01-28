39e55b2961634217b07924fb8a50144b29586657
brunomacedo <contato@brunomacedo.com.br>
2022-01-28 17:09:59 -0300

    fix: update background color

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -8,6 +8,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 16,
+    backgroundColor: '#fff',
   },
 });
 
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -9,6 +9,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 16,
+    backgroundColor: '#fff',
   },
 });
 
