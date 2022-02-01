c01c63f3d7c8c50fd15e50fff54a9e34fdb723c1
brunomacedo <contato@brunomacedo.com.br>
2022-02-01 16:37:21 -0300

    fix(tabs): set default menu items

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,37 +1,7 @@
 import React from 'react';
 import {ScrollView, Text, View} from 'react-native';
+import {MenuItems} from '../../services/menu.services';
 import TabStyles from './styles';
-
-import {IconStore} from '../Icons/store';
-import {IconOffers} from '../Icons/offers';
-
-const TabItems = [
-  {
-    key: 1,
-    icon: <IconStore />,
-    text: 'Loja',
-  },
-  {
-    key: 2,
-    icon: <IconOffers />,
-    text: 'Ofertas',
-  },
-  {
-    key: 3,
-    icon: <IconStore />,
-    text: 'Cupons',
-  },
-  {
-    key: 4,
-    icon: <IconOffers />,
-    text: 'Ajuda',
-  },
-  {
-    key: 5,
-    icon: <IconStore />,
-    text: 'Conta',
-  },
-];
 
 const Tabs: React.FC = () => {
   return (
@@ -40,7 +10,7 @@ const Tabs: React.FC = () => {
         contentContainerStyle={TabStyles.tabScroll}
         showsHorizontalScrollIndicator={false}
         horizontal>
-        {TabItems.map(item => (
+        {MenuItems.map(item => (
           <View key={item.key} style={TabStyles.tabItem}>
             {item.icon}
             <Text numberOfLines={2} style={TabStyles.tabText}>
--- src/components/Tabs/styles.ts
+++ src/components/Tabs/styles.ts
@@ -4,28 +4,23 @@ import {colors} from '../../theme/styles';
 const TabStyles = StyleSheet.create({
   container: {
     height: 80 + 32,
-    marginTop: 20,
-    backgroundColor: colors.philippine,
+    // marginTop: 16,
     paddingTop: 16,
     paddingBottom: 16,
   },
   tabScroll: {
     alignItems: 'center',
     paddingRight: 16,
+    paddingLeft: 6,
   },
   tabItem: {
     width: 80,
     height: '100%',
     borderRadius: 3,
-    marginLeft: 16,
+    marginLeft: 10,
     padding: 10,
     justifyContent: 'space-between',
-    backgroundColor: `${String(colors.white)}20`,
-  },
-  tabIcon: {
-    width: 24,
-    height: 24,
-    backgroundColor: `${String(colors.white)}20`,
+    backgroundColor: `${String(colors.white)}40`,
   },
   tabText: {
     fontSize: 13,
