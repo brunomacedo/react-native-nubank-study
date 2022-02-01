d9ab7eb11ba04b85a06078990bc384008894355d
brunomacedo <contato@brunomacedo.com.br>
2022-02-01 16:38:07 -0300

    feat(main): import components

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import {ScrollView, View} from 'react-native';
+import {View} from 'react-native';
+import ContentCard from '../../components/ContentCard';
 import Header from '../../components/Header';
+import Menu from '../../components/Menu';
 import Tabs from '../../components/Tabs';
 import styles from '../../theme/styles';
+import MainStyles from './styles';
 
 const Main: React.FC = () => {
   return (
-    <ScrollView
-      style={styles.scroll}
-      bounces={false}
-      showsVerticalScrollIndicator={false}>
-      <View style={styles.container}>
-        <Header />
-        <Tabs />
+    <View style={styles.container}>
+      <Header />
+
+      <View style={MainStyles.content}>
+        <Menu />
+        <ContentCard />
       </View>
-    </ScrollView>
+
+      <Tabs />
+    </View>
   );
 };
 
---
+++ src/pages/Main/styles.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+const MainStyles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    maxHeight: 400,
+    zIndex: 5,
+  },
+});
+
+export default MainStyles;
